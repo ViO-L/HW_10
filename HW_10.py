22cdc25242452d028cac745ad0e157d456afb1e5
lomak <lomako221@mail.ru>
2024-02-18 23:19:30 +0300

    added code HW_10 and add inf README

--- HW_10.py
+++ HW_10.py
@@ -7,9 +7,6 @@ lst += ['human'] * 10
 random.shuffle(lst)
 data = pd.DataFrame({'whoAmI':lst})
 data.head()
-
-
-
 '''
 import pandas as pd
 import random
@@ -21,7 +18,7 @@ unique_val = data['whoAmI'].unique()
 
 
 for val in unique_val:
-    data[val] = (data['whoAmI'] == val).astype(int) #true = 1 false = 0
+    data[val] = (data['whoAmI'] == val).astype(int)
 data = data.drop('whoAmI', axis=1)
 
 print(data)
