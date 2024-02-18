eb0c98379eb700eac39c1d1c8994c97a580a613d
lomak <lomako221@mail.ru>
2024-02-18 23:09:21 +0300

    added file HW_10

---
+++ HW_10.py
@@ -0,0 +1,27 @@
+'''В ячейке ниже представлен код генерирующий DataFrame, которая состоит всего из 1 столбца. Ваша задача перевести его в one hot вид.
+
+Сможете ли вы это сделать без get_dummies?
+import random
+lst = ['robot'] * 10
+lst += ['human'] * 10
+random.shuffle(lst)
+data = pd.DataFrame({'whoAmI':lst})
+data.head()
+
+
+
+'''
+import pandas as pd
+import random
+
+lst = ['robot'] * 10 + ['human'] * 10
+random.shuffle(lst)
+data = pd.DataFrame({'whoAmI':lst})
+unique_val = data['whoAmI'].unique()
+
+
+for val in unique_val:
+    data[val] = (data['whoAmI'] == val).astype(int) #true = 1 false = 0
+data = data.drop('whoAmI', axis=1)
+
+print(data)
